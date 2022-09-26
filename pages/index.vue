86eb9015e99b1e129628d50f26ae7d1e4e3abd90
Xceldeveloper <56355878+Xceldeveloper@users.noreply.github.com>
2022-09-26 23:43:13 +0100

    fix: husky

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,30 @@
 <template>
-  <div>
-    {{ message }}
-  </div>
+  <div> {{ message }} ... </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+  import Vue from "vue";
 
-export default Vue.extend({
-  name: 'IndexPage',
-  data() {
-    return {
-      message: '',
+  export default Vue.extend({
+    name: "IndexPage",
+    data() {
+      return {
+        message: ""
+      };
+    },
+
+    async mounted() {
+      // axios post to api/express and console.log the response
+      const response = await this.$axios.post("/api/express", {
+        info: "hello"
+      });
+      this.message = response.data;
+      // const sendEmail = await this.$axios.post('/api/send-email', {
+      //   email: 'weirdfalcao@gmail.com',
+      //   name: 'ov',
+      //   message: 'hello from nuxt...',
+      // })
+      // console.log(JSON.stringify(sendEmail, null, 2))
     }
-  },
-
-  async mounted() {
-    // axios post to api/express and console.log the response
-    const response = await this.$axios.post('/api/express', { info: 'hello' })
-    this.message = response.data
-    // const sendEmail = await this.$axios.post('/api/send-email', {
-    //   email: 'weirdfalcao@gmail.com',
-    //   name: 'ov',
-    //   message: 'hello from nuxt...',
-    // })
-    // console.log(JSON.stringify(sendEmail, null, 2))
-  },
-})
+  });
 </script>
